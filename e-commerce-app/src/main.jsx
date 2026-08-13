9d7117f82e2c45194193427989b86021e804b79a
mavurimahalakshmi2006-ux <mavurimahalakshmi2006@gmail.com>
2026-08-13 15:46:17 +0530

    add the bootstrap link to the index.html

--- e-commerce-app/src/main.jsx
+++ e-commerce-app/src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
